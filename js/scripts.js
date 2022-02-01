8dc8f4d1d52266228bda7d24b64dea2999ff6887
Nassif36 <xsrkhalil@gmail.com>
2022-02-01 11:54:12 -0300

    navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,44 @@
 
+
 // Navbar on scroll
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+    var currentScrollPos = window.pageYOffset;
+    var barraFixed = document.getElementById("barra");
 
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function () {
-        var currentScrollPos = window.pageYOffset;
-        var barraFixed = document.getElementById("barra");
+    if (prevScrollpos > currentScrollPos) {
+        barraFixed.style.top = "0";
+        barraFixed.style.background = "#faf8f8";
+        barraFixed.classList.add('fixed')
 
-        if (currentScrollPos >= 600) {
+    } else {
+        barraFixed.style.top = "-54vh";
+    }
+    if (currentScrollPos >= 5) {
+        barraFixed.style.background = "#faf8f8";
 
-
-            barraFixed.classList.add('fixed')
-            barraFixed.style.top = "0";
-
-        } else {
-            barraFixed.classList.remove('fixed')
-        }
+    } else {
+  
+        barraFixed.classList.remove('fixed')
 
 
     }
+    prevScrollpos = currentScrollPos;
+}
 
+// Mobile menu
+var menu = document.querySelector('.navigation-links');
+var menuBtn = document.querySelectorAll(".open-btn");
+
+menuBtn.forEach(menuBtn => menuBtn.addEventListener('click', () => {
+     menu.classList.toggle('opened');
+     menuBtn.classList.toggle('opened');
+}));
+
+var menuLink = document.querySelectorAll(".nav_links li");
+menuLink.forEach(menuLink => menuLink.addEventListener('click', () => {
+    menu.classList.toggle('opened');
+}));
 
 
     const enlaces = document.querySelectorAll('.enlace a');
@@ -36,20 +56,7 @@
 
 
 
-// Mobile hamburguer
 
-var menu = document.querySelector('.site-navigation');
-var menuBtn = document.querySelector(".bars");
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('opened');
-    menuBtn.classList.toggle('opened');
-});
-var menuLink = document.querySelectorAll(".nav-links li");
-menuLink.forEach(menuLink => menuLink.addEventListener('click', () => {
-    menu.classList.toggle('opened');
-    menuBtn.classList.toggle('opened');
-}));
 
 
 // Observer
