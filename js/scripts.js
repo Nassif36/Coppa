34ecf234c05be4395eaa8a571048223fa629c3c8
khalil <xsrkhalil@gmail.com>
2025-02-14 20:04:59 -0300

    form submit & layout fix

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@
 
 // Navbar on scroll
 
+let headerElement = document.querySelector('.header-spacing ')
+headerElement.style.paddingTop = document.getElementById('barra').offsetHeight + 'px'
 
 // Mobile menu
 var menu = document.querySelector('.navigation-links');
@@ -151,67 +153,3 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 }, false);
 
-
-
-
-
-// function getSubmitData, send the data to the email server
-
-
-
-const getSubmitData = () => {
-    event.preventDefault()
-    let name = document.getElementById('form-name').value;
-    let number = document.getElementById('form-number').value;
-    let to = document.getElementById('form-mail').value;
-    let subject = document.getElementById('form-asunto').value;
-    let body = document.getElementById('form-mensaje').value;
-    if(name.value <= 0) {
-        alert('completa los campos')
-    } else {
-        
-    }
-    //show the loading animation
-    document.getElementById('loading').style.display = 'block';
-
-    let url = "https://go-mails.herokuapp.com/email";
-    let data = {
-        name,
-        number,
-        to,
-        subject,
-        body,
-    };
-
-    
-
-    fetch(url, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    })
-        .then((res) => res.json())
-        .then((res) => {
-            if (res.status === 'ok') {
-                document.getElementById('form-name').value = '';
-                document.getElementById('form-number').value = '';
-                document.getElementById('form-mail').value = '';
-                document.getElementById('form-asunto').value = '';
-                document.getElementById('form-mensaje').value = '';
-                document.getElementById('loading').style.display = 'none';
-                document.getElementById('my-form-status').innerHTML = 'Mensaje enviado correctamente';
-                 document.getElementById('my-form-status').style.background.color = '#008000';
-                 document.getElementById('my-form-status').style.border.radius = '3px';
-                 document.getElementById('my-form-status').style.maxWidth = '450px';
-            } else {
-     
-                 document.getElementById('my-form-status').style.color = '#008000';
-                 document.getElementById('my-form-status').style.border.radius = '3px';
-                 document.getElementById('my-form-status').style.padding = '1em 2em';
-                document.getElementById('my-form-status').innerHTML = 'Ooops.. ocurrio un error estamos trabajando en ello';
-            }
-        });
-
-};
